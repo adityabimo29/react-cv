14fab72f5b80bda4e95a00fd243f7f5d2e2032b5
adityabimo29 <porusruby@gmail.com>
2020-01-22 09:20:44 +0700

    fix footer

--- src/Footer.js
+++ src/Footer.js
@@ -3,7 +3,7 @@ import React from 'react';
 function Footer(){
     return(
         <footer className='pv4 ph3 ph5-m ph6-l mid-gray'>
-            <small className='f6 db tc'>© 2020 <b className="ttu">Raccon Inc.</b> , All Rights Reserved</small>
+            <small className='f6 db tc'>© 2020 <b className="ttu">Racoon Inc.</b> , All Rights Reserved</small>
             <div className="tc mt3">
                 <a href="https://instagram.com" title="Language" className="f6 dib ph2 link mid-gray dim">Instagram</a>
                 <a href="https://facebook.com"    title="Terms" className="f6 dib ph2 link mid-gray dim">Facebook</a>
